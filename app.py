f1d7319c06cb0e31e6f1fcc0dc7886fd57d21344
Sankeerth <ssr.22u10038@btech.nitdgp.ac.in>
2025-04-30 15:05:57 +0530

    feature: audio

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from typing import Union
 from tools import process_input
-
+from llm import process_url
 
 app = FastAPI()
 
@@ -16,3 +16,10 @@ def read_query(q: Union[str, None] = None):
         return {"papers": result}
     return {"query": "No query provided"}
 
+@app.get("/create_podcast")
+def create_podcast(url: Union[str, None] = None):
+    if url:
+        script = process_url(url)
+        file_path = "podcast.mp3"
+        
+    return {"query": "No query provided"}
